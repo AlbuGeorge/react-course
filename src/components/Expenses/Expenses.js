f3af0402d392b27840ab51e84513a7d6b5a167f4
George Albu <george.albu1@gmail.com>
2022-09-21 16:51:35 +0300

    filtered

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,11 +10,25 @@ const Expenses = (props) => {
   const saveValue = (selectedYear) => {
     setFilteredYear(selectedYear)
   }
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter onSaveValue={saveValue} selected={filteredYear} />
-        <ExpenseItem
+        {filteredExpenses.map((el) => {
+          return (
+            <ExpenseItem
+              key={el.id}
+              title={el.title}
+              amount={el.amount}
+              date={el.date}
+            />
+          )
+        })}
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -33,7 +47,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   )
